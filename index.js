abeeb09498a7dbe2eee9a39ce650104bfc791407
pyrhaa <sboubtane@protonmail.com>
2022-01-22 15:27:40 +0100

    fullstack end, go database

--- index.js
+++ index.js
@@ -92,8 +92,8 @@ app.post('/api/persons', (req, res) => {
   res.json(person);
 });
 
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' });
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' });
 };
 
 app.use(unknownEndpoint);
